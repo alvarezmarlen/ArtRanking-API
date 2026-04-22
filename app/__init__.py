8c2d7ccfc467b0d05af16b99cef80deeb966037e
Juan Carlos <juancarlosgilv@gmail.com>
2026-04-22 11:39:58 +0200

    correccion errores: creacion Votaciones, comentarios y notificaciones

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,7 @@ from app.extensiones.db import init_db
 from mongoengine.connection import get_db
 from app.routes.auth_rutas import auth_bp
 from app.routes.usuario_rutas import usuario_bp
-
+from app.routes.voto_rutas import voto_bp
 
 def create_app(config_class=DevelopmentConfig):
    app = Flask(__name__)
@@ -12,7 +12,8 @@ def create_app(config_class=DevelopmentConfig):
 
    app.register_blueprint(auth_bp, url_prefix="/auth")
    app.register_blueprint(usuario_bp, url_prefix="/users")
-   
+   app.register_blueprint(voto_bp, url_prefix="/votos")
+
    # Inicializar DB
    init_db(app)
 
